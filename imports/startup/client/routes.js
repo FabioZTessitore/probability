2f8f6f93cd4e7403afe080419711412c65b146dd
Fabio Z Tessitore <fabioztessitore@gmail.com>
2016-08-21 16:40:02 +0200

    NotFound Template

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -1,8 +1,15 @@
 import { FlowRouter } from 'meteor/kadira:flow-router';
 import { BlazeLayout } from 'meteor/kadira:blaze-layout';
 
+import '../../ui/templates/not_found.html';
 import '../../ui/templates/home/home.js';
 import '../../ui/layouts/main_layout.js';
+
+FlowRouter.notFound = {
+  action() {
+    BlazeLayout.render('MainLayout', { content: 'NotFound' });
+  },
+};
 
 FlowRouter.route('/', {
   name: 'home-page',
