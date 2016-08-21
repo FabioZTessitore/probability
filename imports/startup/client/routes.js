831d9ee5979115f3a4d9352693ddd11c906e7e14
Fabio Z Tessitore <fabioztessitore@gmail.com>
2016-08-21 16:29:18 +0200

    signup/login/logout

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -2,6 +2,8 @@ import { FlowRouter } from 'meteor/kadira:flow-router';
 import { BlazeLayout } from 'meteor/kadira:blaze-layout';
 
 import '../../ui/templates/not_found.html';
+import '../../ui/templates/signup/signup.js';
+import '../../ui/templates/login/login.js';
 import '../../ui/templates/home/home.js';
 import '../../ui/layouts/main_layout.js';
 
@@ -15,5 +17,19 @@ FlowRouter.route('/', {
   name: 'home-page',
   action() {
     BlazeLayout.render('MainLayout', { content: 'Home' });
+  },
+});
+
+FlowRouter.route('/signup', {
+  name: 'signup',
+  action() {
+    BlazeLayout.render('MainLayout', { content: 'Signup' });
+  },
+});
+
+FlowRouter.route('/login', {
+  name: 'login',
+  action() {
+    BlazeLayout.render('MainLayout', { content: 'Login' });
   },
 });
