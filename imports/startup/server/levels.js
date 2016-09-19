306f401d7f10275e588f53839f1c37d8d05ed1bb
Fabio Z Tessitore <fabioztessitore@gmail.com>
2016-09-19 21:26:47 +0200

    startup

--- imports/startup/server/levels.js
+++ imports/startup/server/levels.js
@@ -1,5 +1,5 @@
 import { Levels } from '../../api/levels/levels.js';
 
 if (Levels.find().count()===0) {
-  Levels.insert({ type: 'City Hall', level: 1, stone: 500, wood: 500, changeProbReproduce: 1, changeProbDie: -5 });
+  Levels.insert({ buildingType: 'City Hall', buildingLevel: 1, stoneNeededToBuild: 100, woodNeededToBuild: 100, changeScaleProbReproduce: 1, changeScaleProbDie: -5 });
 }
