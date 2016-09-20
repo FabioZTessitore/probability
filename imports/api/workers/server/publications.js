ff7a368d937a2c1d377d1ce7945f80ccf59cbcae
Fabio Z Tessitore <fabioztessitore@gmail.com>
2016-09-20 20:51:07 +0200

    update()

--- imports/api/workers/server/publications.js
+++ imports/api/workers/server/publications.js
@@ -1,6 +1,10 @@
 import { Meteor } from 'meteor/meteor';
 import { Workers } from '../workers.js';
 
+Meteor.publish('workers', function () {
+  return Workers.find({ owner: this.userId });
+});
+
 Meteor.publish('workersOfCity', function (cityId) {
   check(cityId, String);
 
