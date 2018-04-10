6d6680ed2bca60d3f012b7b50c117bc274fb1612
Fabio Z Tessitore <fabioztessitore@gmail.com>
2018-04-10 21:57:21 +0200

    signin, signup. only pages

--- node/app/guest.js
+++ node/app/guest.js
@@ -1,60 +1,41 @@
 const express = require('express');
+const path = require('path');
+
 const router = express.Router();
 
 const authUtils = require('./auth_utils');
 
 module.exports = function () {
 
-  // Landing Page
+  // Landing Page and Sign In Page
+  //
+  // Already logged in user cannot
+  // reach these pages
+
+  // Landing Page (with Sign Up form)
   router.get('/', function (req, res) {
-    res.send('landing page');
-    /*
-    res.render('index.ejs', {
-      login: authUtils.boolIsLoggedIn(req),
-      role: authUtils.role(req),
-    });
-    */
+    res.sendFile( path.resolve( __dirname, '..', 'public', 'index.html' ) );
   });
 
-  // Sign In e Sign Up
-  // Nel caso di utenti gia' loggati
-  // le pagine risultano irraggiungibili
-
   // Sign In Page
-  /*
   router.get('/signin', function (req, res) {
+    /*
     const loggedIn = authUtils.boolIsLoggedIn(req);
     
     if (loggedIn) {
-      res.redirect('/');
+      res.redirect('/home');
       return;
     }
-
+    */
+    res.sendFile( path.resolve( __dirname, '..', 'public', 'signin.html' ) );
+    /*
     res.render('signin.ejs', {
       login: loggedIn,
       message: req.flash('loginMessage'),
       role: authUtils.role(req),
     });
+    */
   });
-  */
-
-  // Sign Up Page
-  /*
-  router.get('/signup', function (req, res) {
-    const loggedIn = authUtils.boolIsLoggedIn(req);
-    
-    if (loggedIn) {
-      res.redirect('/');
-      return;
-    }
-
-    res.render('signup.ejs', {
-      login: loggedIn,
-      message: req.flash('signupMessage'),
-      role: authUtils.role(req),
-    });
-  });
-  */
 
   return router;
 };
